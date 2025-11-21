fe828fd649b370ddad56897feea0ecff7e009d44
Manuel-Campoy <mcampoyteran5@gmail.com>
2025-11-21 11:25:33 -0700

    imports

--- video10/react-youtube/src/main.jsx
+++ video10/react-youtube/src/main.jsx
@@ -1,7 +1,14 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
+import React from 'react';
 
 const container = document.getElementById('root');
 
-ReactDOM.render(<App />, container);
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
